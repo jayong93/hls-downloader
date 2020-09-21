3334bab0f59a82afa92eeb46afc27ba87b1c6561
최재용 <jayong93@gmail.com>
2020-09-22 00:17:19 +0900

    update lib search path on Windows

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::env;
 fn main() {
     if env::var("CARGO_CFG_WINDOWS").is_ok() {
-        println!(r"cargo:rustc-link-search=C:\gstreamer-win\1.0\x86_64\lib")
+        println!(r"cargo:rustc-link-search=C:\gstreamer\1.0\msvc_x86_64\lib")
     }
 }
