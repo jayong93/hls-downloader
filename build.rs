67d99e4709c126ef74cfa3ed10351c76b37ee622
최재용 <jayong93@gmail.com>
2020-10-10 01:43:16 +0900

    prevent recompiling always because of the build script

--- build.rs
+++ build.rs
@@ -3,4 +3,5 @@ fn main() {
     if env::var("CARGO_CFG_WINDOWS").is_ok() {
         println!(r"cargo:rustc-link-search=C:\gstreamer\1.0\msvc_x86_64\lib")
     }
+    println!("cargo:rerun-if-changed=build.rs");
 }
